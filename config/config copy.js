bf9615e1e0e2ee4aed8d6196e4d172d77760aa77
zhangyaohuang <zhangyaohuang@vv.cn>
2020-08-27 10:19:32 +0800

    initial commit

--- config/config copy.js
+++ config/config copy.js
@@ -1,11 +1,11 @@
-var os = require("os");
+var os = require('os');
 
 var config = {};
 config.development = {
   // Config for database, only support mysql.
   db: {
     username: process.env.RDS_USERNAME || "root",
-    password: process.env.RDS_PASSWORD || "123456",
+    password: process.env.RDS_PASSWORD || "rootroot",
     database: process.env.DATA_BASE || "codepush",
     host: process.env.RDS_HOST || "127.0.0.1",
     port: process.env.RDS_PORT || 3306,
@@ -18,7 +18,7 @@ config.development = {
     accessKey: "",
     secretKey: "",
     bucketName: "",
-    downloadUrl: "", // Binary files download host address.
+    downloadUrl: "" // Binary files download host address.
   },
   // Config for upyun (https://www.upyun.com/) storage when storageType value is "upyun"
   upyun: {
@@ -57,19 +57,16 @@ config.development = {
   // Config for local storage when storageType value is "local".
   local: {
     // Binary files storage dir, Do not use tmpdir and it's public download dir.
-    storageDir: process.env.STORAGE_DIR || "/home/ubuntu/codepush/storage",
+    storageDir: process.env.STORAGE_DIR || "/Users/codepush/storage",
     // Binary files download host address which Code Push Server listen to. the files storage in storageDir.
-    downloadUrl:
-      process.env.LOCAL_DOWNLOAD_URL || "http://188.131.160.136:3000",
+    downloadUrl: process.env.LOCAL_DOWNLOAD_URL || "http://172.16.57.160:3000",
     // public static download spacename.
-    public: "",
+    public: ''
   },
   jwt: {
     // Recommended: 63 random alpha-numeric characters
     // Generate using: https://www.grc.com/passwords.htm
-    tokenSecret:
-      process.env.TOKEN_SECRET ||
-      "seQlbAmYpToeL092tFsosvSePPXaNgRtdYd0bJDvpri9u7TUnOaOKBaPUlebuLX",
+    tokenSecret: process.env.TOKEN_SECRET ||'seQlbAmYpToeL092tFsosvSePPXaNgRtdYd0bJDvpri9u7TUnOaOKBaPUlebuLX'
   },
   common: {
     /*
@@ -83,7 +80,7 @@ config.development = {
     // create patch updates's number. default value is 3
     diffNums: 3,
     // data dir for caclulate diff files. it's optimization.
-    dataDir: "/home/ubuntu/codepush/data",
+    dataDir: '/Users/codepush/data',
     // storageType which is your binary package files store. options value is ("local" | "qiniu" | "s3"| "oss" || "tencentcloud")
     storageType: process.env.STORAGE_TYPE || "local",
     // options value is (true | false), when it's true, it will cache updateCheck results in redis.
@@ -92,14 +89,14 @@ config.development = {
     rolloutClientUniqueIdCache: false,
   },
   // Config for smtp email，register module need validate user email project source https://github.com/nodemailer/nodemailer
-  smtpConfig: {
+  smtpConfig:{
     host: "smtp.aliyun.com",
     port: 465,
     secure: true,
     auth: {
       user: "",
-      pass: "",
-    },
+      pass: ""
+    }
   },
   // Config for redis (register module, tryLoginTimes module)
   redis: {
@@ -107,33 +104,33 @@ config.development = {
       host: "127.0.0.1",
       port: 6379,
       retry_strategy: function (options) {
-        if (options.error.code === "ECONNREFUSED") {
+        if (options.error.code === 'ECONNREFUSED') {
           // End reconnecting on a specific error and flush all commands with a individual error
-          return new Error("The server refused the connection");
+          return new Error('The server refused the connection');
         }
         if (options.total_retry_time > 1000 * 60 * 60) {
-          // End reconnecting after a specific timeout and flush all commands with a individual error
-          return new Error("Retry time exhausted");
+            // End reconnecting after a specific timeout and flush all commands with a individual error
+            return new Error('Retry time exhausted');
         }
         if (options.times_connected > 10) {
-          // End reconnecting with built in error
-          return undefined;
+            // End reconnecting with built in error
+            return undefined;
         }
         // reconnect after
         return Math.max(options.attempt * 100, 3000);
-      },
-    },
-  },
-};
+      }
+    }
+  }
+}
 
 config.development.log4js = {
-  appenders: { console: { type: "console" } },
-  categories: {
-    default: { appenders: ["console"], level: "error" },
-    startup: { appenders: ["console"], level: "info" },
-    http: { appenders: ["console"], level: "info" },
-  },
-};
+  appenders: {console: { type: 'console'}},
+  categories : {
+    "default": { appenders: ['console'], level:'error'},
+    "startup": { appenders: ['console'], level:'info'},
+    "http": { appenders: ['console'], level:'info'}
+  }
+}
 
 config.production = Object.assign({}, config.development);
 module.exports = config;
